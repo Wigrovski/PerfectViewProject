7e7b456a6fd5602ef00555f12e5dbbe083235d82
Alexandr <kobal2009@yandex.ru>
2023-01-15 13:38:09 +0500

    Create helpers & change animate in modal (lesson25)

--- src/modules/modal.js
+++ src/modules/modal.js
@@ -1,30 +1,14 @@
+import { animate } from "./helpers"
+
 const modal = () => {
     const modal = document.querySelector('.popup')
     const buttons = document.querySelectorAll('.popup-btn')
-
-    function animate({timing, draw, duration}) {
-
-        let start = performance.now()
-        requestAnimationFrame(function animate(time) {
-        
-        // timeFraction изменяется от 0 до 1
-        let timeFraction = (time - start) / duration;
-        if (timeFraction > 1) timeFraction = 1
-
-        // вычисление текущего состояния анимации
-        let progress = timing(timeFraction);
-            draw(progress); // отрисовать её
-        if (timeFraction < 1) {
-            requestAnimationFrame(animate);
-        }    
-    });
-    }
 
     buttons.forEach(btn => {
         btn.addEventListener('click', () => {
             if(document.documentElement.clientWidth > 768) {
                 animate({
-                    duration: 20,
+                    duration: 80,
                     timing(timeFraction) {
                         return timeFraction
                     },
@@ -36,12 +20,13 @@ const modal = () => {
                             modal.style.opacity = '1'
                         })
                     }
-                }, 10)
+                }, 50)
             }
             modal.style.display = 'block'
         })
         
     })
+    //Делегирование события закрытия  модального окна (урок 22)
     modal.addEventListener('click', (e) => {
         if (!e.target.closest('.popup-content') || e.target.classList.contains('popup-close')) {
             modal.style.display = 'none'
