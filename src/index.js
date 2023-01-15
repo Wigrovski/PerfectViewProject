87bf26843f834288c20360a4208646f9e37ad777
Alexandr <kobal2009@yandex.ru>
2023-01-15 17:42:44 +0500

    Sendform first commit

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import validform from './modules/validform'
 import tabs from './modules/tabs'
 import slider from './modules/slider'
 import calc from './modules/calc'
+import sendForm from './modules/sendForm'
 
 
 
@@ -15,4 +16,30 @@ validform()
 tabs()
 slider()
 calc(1000)
+sendForm({
+    formId: 'form1',
+    someElem: [
+    {
+        type: 'block',
+        id: 'total'
+    }]
+});
+sendForm({
+    formId: 'form2',
+    someElem: [
+    {
+        type: 'block',
+        id: 'total'
+    }]
+});
+sendForm({
+    formId: 'form3',
+    someElem: [
+    {
+        type: 'block',
+        id: 'total'
+    }
+    ]
+});
+
 
---
+++ src/modules/sendForm.js
@@ -0,0 +1,86 @@
+const sendForm = ({formId, someElem = []}) => {
+    const form = document.getElementById(formId)
+    const statusBlock = document.createElement('div')
+    const loadText = 'Загрузка...'
+    const errorText = 'Ошибка...'
+    const successText = 'Спасибо наш менеджер свяжется с вами'
+
+
+    const validate = (list) => {
+        let success = true
+
+        list.forEach((input) => {
+            if (
+                ((input.type === "text") && (input.value.length < 2)) ||
+                ((input.type === "email") && ((!input.value.includes('@') || (!input.value.includes('.'))))) ||
+                ((input.type === "tel") && ((input.value.length < 12)))) {
+                success = false
+            }
+        })
+        return success
+    }
+
+    const sendData = (data) => {
+        return fetch('https://jsonplaceholder.typicode.com/posts', {
+            method: 'POST',
+            body: JSON.stringify(data),
+            headers: {
+                "Content-Type": "application/json"
+            }
+        }).then(res => res.json())
+    }
+
+    const submitForm = () => {
+        const formElement = document.querySelectorAll('input')
+        const formData = new FormData(form)
+        const formBody = {}
+
+        statusBlock.textContent = loadText
+        form.append(statusBlock)
+
+        formData.forEach((val, key) => {
+            formBody[key] = val
+        })
+
+        someElem.forEach(elem => {
+            const element = document.getElementById(elem.id)
+            if (elem.type === 'block') {
+                formBody[elem.id] = element.textContent
+            } else if (elem.type === 'input') {
+                formBody[elem.id] = element.value
+            }
+        }) 
+
+        if (validate(formElement)) {
+            sendData(formBody).then((data) => {
+                statusBlock.textContent = successText
+                formElement.forEach((input) => {
+                    input.value = ''
+                })
+            })
+            .catch ((error) => {
+                statusBlock.textContent = errorText
+            })
+        } else { 
+            alert('Данные не верны')
+            statusBlock.textContent = errorText
+        }
+
+    }
+
+    try {
+        if(!form) {
+            throw new Error('Форма отсутствует')
+        }
+        form.addEventListener('submit', (e) => {
+            e.preventDefault()
+            submitForm()
+            
+        })
+    } catch(error) {
+        console.log(error.message);
+
+    }
+}
+
+export default sendForm
